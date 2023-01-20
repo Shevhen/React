fb6176b7b82551293242c2803a7c6fcffdb1a65e
Shevhen <Shevhen9@gmail.com>
2023-01-20 01:44:25 +0100

    home work react lesson1

--- src/App.js
+++ src/App.js
@@ -1,25 +1,39 @@
-import logo from './logo.svg';
 import './App.css';
+import {useEffect, useState} from "react";
+import {RikiMortyFetch} from "./resurs/RikiMortyFetch";
+import {simpsonsArr} from "./resurs/Simpsons";
+import {RikiMorty, Simpsons} from "./components/Heroes";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// 1. Описати всю сім'ю сімпсонів (5 персонажів)
+//
+// 2. Створити компонент який описує персонажа (властивості id,name,status,species,gender,image) з цього апі
+// https://rickandmortyapi.com/
+//     https://rickandmortyapi.com/api/character
+//         Створити 6 персонажів
+
+function App(){
+    let [heroes, setHeroes] = useState([])
+    useEffect(()=>{
+        RikiMortyFetch().then(value => {
+            value.results.splice(6,20)
+            setHeroes(value.results)})},[])
+    return(
+        <div>
+            <div className='simpson'>
+                <h1>Simpsons</h1>
+                {
+                    simpsonsArr.map((value,index) => ({id: index+1,name:value.name,surname:value.surname,age:value.age,photo:value.photo,info:value.info }))
+                                .map(value => <Simpsons key={value.id} value={value}/>)
+                }
+            </div>
+            <div className='rikiMorty'>
+                <h1>Rick & Morty</h1>
+                {
+                    heroes.map(value => <RikiMorty key={value.id} value={value}/>)
+                }
+            </div>
+        </div>
+    )
 }
-
 export default App;
+
